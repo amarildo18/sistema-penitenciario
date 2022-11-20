61855379a2385561e7e478150223eaf085c1d05e
Amarildo Ferreira <abreu-ferreira18@outlook.com>
2022-11-20 19:33:37 +0000

    Adicionando o componente sidebar

--- sis-penitenciario/src/app/app-routing.module.ts
+++ sis-penitenciario/src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 const routes: Routes = [
   {
     path:'login',
     component:LoginComponent
+  },
+  {
+    path:'home',
+    component:HomeComponent
   }
 ];
 
--- sis-penitenciario/src/app/app.module.ts
+++ sis-penitenciario/src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './components/login/login.component';
-import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from './components/main-layout/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { SidebarComponent } from './components/main-layout/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
